506e808819459da3c87c0067f5f831b8ba683a75
Gene Boggs <gene@ology.net>
2025-09-19 20:20:26 -0700

    WIP

--- src/music_voicegen/__init__.py
+++ src/music_voicegen/__init__.py
@@ -1,5 +1,3 @@
-# The root of the entire project
 from music_voicegen.music_voicegen import MusicVoiceGen
 
-# This is the version number of your code. This will change each new release.
-__version__ = "0.1.0"
+__version__ = "0.1.1"
