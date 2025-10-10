22a8e3708f7ee46276082f21f3011118c72461e5
Gene Boggs <gene@ology.net>
2025-10-09 18:54:31 -0700

    WIP

--- examples/transition.py
+++ examples/transition.py
@@ -1,5 +1,5 @@
 import random
-from music21 import converter, corpus, note, stream
+from music21 import converter, corpus, instrument, note, stream
 # if author:
 import sys
 sys.path.append('./src')
@@ -10,6 +10,8 @@ from music_voicegen.music_voicegen import MusicVoiceGen
 # song = converter.parse('/Users/gene/Music/MIDI/lichens_g_major.mid')
 # song = corpus.parse('bwv66.6')
 song = corpus.parse('bwv1.6')
+song = instrument.partitionByInstrument(song)[3]
+# song.show()
 
 max = int(sys.argv[1]) if len(sys.argv) > 1 else 16 # maximum notes in the result phrase
 
@@ -18,6 +20,7 @@ prev = None # network item
 last = None # network transition item
 total = 0 # total number of notes
 pitches = []
+intervals = []
 
 # gather the transitions
 for n in song.flatten().notes:
@@ -26,6 +29,9 @@ for n in song.flatten().notes:
         if prev:
             if last:
                 key = (prev.name, last.name)
+                interval = last.pitch.midi - prev.pitch.midi
+                # print(f"{last.name}{last.octave} - {prev.name}{prev.octave} = {interval}")
+                intervals.append(interval)
                 if key in transition:
                     if n.name in transition[key]:
                         transition[key][n.name] += 1
@@ -40,6 +46,7 @@ for n in song.flatten().notes:
             prev = n
 
 pitches = list(set(pitches)) # uniqify
+intervals = list(set(intervals)) # uniqify
 
 # probability for each transition
 for k,v in transition.items():
@@ -68,7 +75,7 @@ while(i < max):
         key = (key[1], n.name)
         i += 1
     else:
-        print(key)
+        # print(key)
         # score.append(note.Rest())
         draw = random.choice(keys)
         key = tuple(draw.split())
