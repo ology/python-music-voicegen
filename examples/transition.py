792f30d7e9f5e61245cd764c85163b427710b7d3
Gene Boggs <gene@ology.net>
2025-10-09 17:29:36 -0700

    WIP

--- examples/transition.py
+++ examples/transition.py
@@ -8,7 +8,8 @@ from music_voicegen.music_voicegen import MusicVoiceGen
 from music_voicegen import MusicVoiceGen
 
 # song = converter.parse('/Users/gene/Music/MIDI/lichens_g_major.mid')
-song = corpus.parse('bwv66.6')
+# song = corpus.parse('bwv66.6')
+song = corpus.parse('bwv1.6')
 
 max = int(sys.argv[1]) if len(sys.argv) > 1 else 16 # maximum notes in the result phrase
 
